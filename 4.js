c884f367f851cf287818cc31dba56af130fca32e
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-09-04 13:42:06 +0300

    homework3 finished

--- 4.js
+++ 4.js
@@ -19,24 +19,23 @@ console.log(sum(2, 6)); должно вывести число 8 в консол
 */
 
 const sum = (numberOne, numberTwo) => {
-  return Number(numberOne + numberTwo);
+    return Number(numberOne + numberTwo);
 }
 const subtraction = (numberOne, numberTwo) => {
-  if (numberOne > numberTwo)
-  {
-    return Number(numberOne - numberTwo);
-  }
-  return Number(numberTwo - numberOne);
+    if (numberOne > numberTwo) {
+        return Number(numberOne - numberTwo);
+    }
+    return Number(numberTwo - numberOne);
 }
 const multiply = (numberOne, numberTwo) => {
-  return Number(numberOne * numberTwo);
+    return Number(numberOne * numberTwo);
 }
 const division = (numberOne, numberTwo) => {
-  return Number(numberOne / numberTwo);
+    return Number(numberOne / numberTwo);
 }
 
 
-console.log(sum(3,2));
-console.log(subtraction(3,2));
-console.log(multiply(3,2));
-console.log(division(3,3));
+console.log(sum(3, 2));
+console.log(subtraction(3, 2));
+console.log(multiply(3, 2));
+console.log(division(3, 3));
